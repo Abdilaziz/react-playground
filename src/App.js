521408b14a0b861d76c941d5eb5059f78246daf2
unknown <azizib2nd@gmail.com>
2019-05-07 23:28:40 -0400

    Add new menu component, add bootstrap card objects

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import { DISHES } from './shared/dishes';
+
 // import ShoppingList from './components/ShoppingList';
 import Game from './components/Game/Game';
+import Menu from './components/MenuComponent/MenuComponent';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -15,6 +27,8 @@ class App extends Component {
             <NavbarBrand href="/">Main Page</NavbarBrand>
           </div>
         </Navbar>
+
+        <Menu  dishes={this.state.dishes} />
 
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
